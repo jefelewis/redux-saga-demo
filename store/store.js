5e93cf81126bb185ec7ce447aa369ebcff97946a
Jeff Lewis <jeffylew77@gmail.com>
2019-07-08 15:39:18 -0700

    [Dev] Adding Redux Store + Redux Saga Middleware

---
+++ store/store.js
@@ -0,0 +1,30 @@
+// Imports: Dependencies
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+
+// Imports: Redux Root Reducer
+import rootReducer from '../reducers/index';
+
+// Imports: Redux Sagas
+import { rootSaga } from '../sagas/index';
+
+// Middleware: Redux Saga
+const sagaMiddleware = createSagaMiddleware();
+
+// Redux: Store
+const store = createStore(
+  rootReducer,
+  applyMiddleware(
+    sagaMiddleware,
+    createLogger()
+  ),
+);
+
+// Middleware: Redux Saga
+sagaMiddleware.run(rootSaga);
+
+// Exports
+export {
+  store,
+}
